99e341831f6ccb9838f5e3c7b4e2106a272cbdff
ChristianChilValencia <valenciamyth@gmail.com>
2025-05-01 04:29:07 +0800

    added tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,26 @@ const routes: Routes = [
   {
     path: 'profile-organization',
     loadChildren: () => import('./profile-organization/profile-organization.module').then( m => m.ProfileOrganizationPageModule)
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  },
+  {
+    path: 'tab5',
+    loadChildren: () => import('./tab5/tab5.module').then( m => m.Tab5PageModule)
+  },
+  {
+    path: 'tab6',
+    loadChildren: () => import('./tab6/tab6.module').then( m => m.Tab6PageModule)
+  },
+  {
+    path: 'tab7',
+    loadChildren: () => import('./tab7/tab7.module').then( m => m.Tab7PageModule)
+  },
+  {
+    path: 'tab8',
+    loadChildren: () => import('./tab8/tab8.module').then( m => m.Tab8PageModule)
   }
 
 ];
