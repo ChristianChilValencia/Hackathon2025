d45ecb72f5b113e6aaabd4d5ea57528ba7d34900
ChristianChilValencia <valenciamyth@gmail.com>
2025-05-01 01:42:45 +0800

    nice

---
+++ src/app/app/welcome-page2/welcome-page2.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-page2',
+  templateUrl: './welcome-page2.page.html',
+  styleUrls: ['./welcome-page2.page.scss'],
+  standalone: false
+})
+export class WelcomePage2Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
