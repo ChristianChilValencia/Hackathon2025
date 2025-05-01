26c99e05a8df8c8f0e3e3e022948a5d016fb7272
WhyMir69 <proto.g.x1@gmail.com>
2025-05-01 14:30:50 +0800

    styled css

--- src/app/welcome-page1/welcome-page1.page.ts
+++ src/app/welcome-page1/welcome-page1.page.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-welcome-page1',
   templateUrl: './welcome-page1.page.html',
   styleUrls: ['./welcome-page1.page.scss'],
   standalone: false
-
 })
 export class WelcomePage1Page implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
-
 }
